2909d9c2ef67bb68c16c6753ba042dda8c10cf2e
Nima Zarei <88039431+n-ii-ma@users.noreply.github.com>
2024-12-10 16:34:51 +0330

    Install `react-native-paper` and its prerequisites and configure it

--- index.js
+++ index.js
@@ -2,8 +2,16 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import { AppRegistry } from 'react-native';
+import { PaperProvider } from 'react-native-paper';
+import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+import App from './App';
+
+const root = () => (
+  <PaperProvider>
+    <App />
+  </PaperProvider>
+);
+
+AppRegistry.registerComponent(appName, () => root);
