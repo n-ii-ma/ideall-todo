c613f5ec00bb67e231fff9bb91edc6e597d0b1c7
Nima Zarei <88039431+n-ii-ma@users.noreply.github.com>
2024-12-10 16:56:08 +0330

    Add bare redux store

--- index.js
+++ index.js
@@ -3,15 +3,20 @@
  */
 
 import { AppRegistry } from 'react-native';
+import { Provider as StoreProvider } from 'react-redux';
 import { PaperProvider } from 'react-native-paper';
+
 import { name as appName } from './app.json';
+import { store } from '@/redux/store';
 
 import App from './App';
 
 const root = () => (
-  <PaperProvider>
-    <App />
-  </PaperProvider>
+  <StoreProvider store={store}>
+    <PaperProvider>
+      <App />
+    </PaperProvider>
+  </StoreProvider>
 );
 
 AppRegistry.registerComponent(appName, () => root);
