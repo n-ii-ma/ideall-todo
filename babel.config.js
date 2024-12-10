9d18db84d8aa5b9f82eac55551f9e2045631222e
Nima Zarei <88039431+n-ii-ma@users.noreply.github.com>
2024-12-10 18:23:46 +0330

    Install `babel-plugin-module-resolver` and update path alias

--- babel.config.js
+++ babel.config.js
@@ -5,4 +5,16 @@ module.exports = {
       plugins: ['react-native-paper/babel'],
     },
   },
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@': './src',
+        },
+      },
+    ],
+  ],
 };
--- index.js
+++ index.js
@@ -7,9 +7,9 @@ import { Provider as StoreProvider } from 'react-redux';
 import { PaperProvider } from 'react-native-paper';
 
 import { name as appName } from './app.json';
-import { store } from '@/redux/store';
+import { store } from './src/redux/store';
 
-import App from '@/app/App';
+import App from './src/app/App';
 
 const root = () => (
   <StoreProvider store={store}>
