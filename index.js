dc8749cca93223c19697bbd8c7b6a4b237214790
Nima Zarei <88039431+n-ii-ma@users.noreply.github.com>
2024-12-11 10:59:05 +0330

    Install and configure `redux-persist` with `Async Storage`

--- index.js
+++ index.js
@@ -4,18 +4,21 @@
 
 import { AppRegistry } from 'react-native';
 import { Provider as StoreProvider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import { PaperProvider } from 'react-native-paper';
 
+import { store, persistor } from './src/redux/store';
 import { name as appName } from './app.json';
-import { store } from './src/redux/store';
 
 import App from './src/app/App';
 
 const root = () => (
   <StoreProvider store={store}>
-    <PaperProvider>
-      <App />
-    </PaperProvider>
+    <PersistGate loading={null} persistor={persistor}>
+      <PaperProvider>
+        <App />
+      </PaperProvider>
+    </PersistGate>
   </StoreProvider>
 );
 
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,12 +1,46 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import todosSliceReducer from './features/todos/todosSlice';
 
-export const store = configureStore({
-  reducer: {
-    todos: todosSliceReducer,
-  },
+// Root reducer
+const rootReducer = combineReducers({
+  todos: todosSliceReducer,
 });
+
+// Persistence config
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage: AsyncStorage,
+};
+
+// Combine and persist reducers
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Store
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+// Create persistor for store
+export const persistor = persistStore(store);
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
